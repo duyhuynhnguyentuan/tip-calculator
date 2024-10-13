6b3aab562e0a4e00e3bd8b5316b97a3fc75fc578
duyhuynhnguyentuan <andyhntd2003@gmail.com>
2024-10-13 15:39:36 +0700

    add result view + amount view

--- tip-calculator/view/ResultView.swift
+++ tip-calculator/view/ResultView.swift
@@ -17,6 +17,81 @@ class ResultView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     private func layout() {
-        backgroundColor = .orange
+        backgroundColor = .white
+        addSubview(vStackView)
+        vStackView.snp.makeConstraints { make in
+            make.top.equalTo(snp.top).offset(24)
+            make.leading.equalTo(snp.leading).offset(24)
+            make.bottom.equalTo(snp.bottom).offset(-24)
+            make.trailing.equalTo(snp.trailing).offset(-24)
+        }
+        horizontalLineView.snp.makeConstraints { make in
+            make.height.equalTo(2)
+        }
+        addShadow(offset: CGSize(width: 0, height: 3),
+                  color: .black,
+                  radius: 12.0,
+                  opacity: 0.1)
+        
     }
+    private let label: UILabel = {
+        LabelFactory.build(text: "Tổng mỗi người:", font: ThemeFont.demibold(ofSize: 18))
+    }()
+    private let amountPerPersonLabel: UILabel = {
+       let label = UILabel()
+        label.textAlignment = .center
+        let text = NSMutableAttributedString(
+            string: "$0",
+            attributes: [
+                .font: ThemeFont.bold(ofSize: 48)
+            ]
+        )
+        text.addAttributes(
+            [
+                .font: ThemeFont.bold(ofSize: 24)
+            ],
+            //start from the position 0 and with the length of 1
+            range: NSMakeRange(0, 1))
+        label.attributedText = text
+        return label
+    }()
+    //seperator view
+    private let horizontalLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = ThemeColor.seperator
+        return view
+    }()
+    private func buildSpacerView(height: CGFloat) -> UIView {
+        let view = UIView()
+        view.heightAnchor.constraint(equalToConstant: height).isActive = true
+        return view
+    }
+    //vertical stack view
+    private lazy var vStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            label,
+            amountPerPersonLabel,
+            horizontalLineView,
+            buildSpacerView(height: 0),
+            hStackView
+        ])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        return stackView
+    }()
+    private lazy var hStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            AmountView(title: "Tổng bill", textAlignment: .left),
+            UIView(),
+            AmountView(title: "Tổng tip", textAlignment: .right)
+        ])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+}
+
+
+#Preview{
+    ResultView()
 }
